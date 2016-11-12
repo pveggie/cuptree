474f9b6bb64202ccaa5e53dfb96daeb0122311a7
Gemma Gotch <pveggie@hotmail.com>
2016-11-12 19:57:46 +0100

    Add functionality for counting cups saved and cups used on the client side

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,1 +1,24 @@
+import { Template } from 'meteor/templating';
+
+import { Cuptrees } from '../../api/cuptrees.js';
+
 import './home.html';
+
+Template.home.helpers({
+  cuptree() {;
+    return Cuptrees.findOne({username: "test"});
+  },
+  // cuptree: { cupsSaved: 1, cupsUsed: 3 },
+});
+
+Template.home.events({
+  'click #use-cup'() {
+    const cuptree = Cuptrees.findOne({username: "test"});
+    Cuptrees.update(cuptree._id, { $set: { cupsUsed: cuptree.cupsUsed + 1 } });
+  },
+  'click #save-cup'() {
+    const cuptree = Cuptrees.findOne({username: "test"});
+    Cuptrees.update(cuptree._id, { $set: { cupsSaved: cuptree.cupsSaved + 1 } });
+  },
+});
+
--- server/main.js
+++ server/main.js
@@ -1,6 +1,1 @@
-// import things required to run the server (e.g server dependencies
-// and both dependencies)
-
-// e.g.
-// import '/imports/startup/server';
-// import '/imports/startup/both';
+import '../imports/startup/server';
